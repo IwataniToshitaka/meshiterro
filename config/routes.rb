af56f0d46b2fb464c13714456b183b63da5c2018
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-11-05 08:01:32 +0000

    [Finish]2-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'homes/about' => 'about#index'
+  get '/about' => 'about#index' ,as: 'about'
   root to: "homes#top"  #サイトのルートページを決める記述
 end
