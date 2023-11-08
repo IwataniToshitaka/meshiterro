5a96f3e644f368579b0cc6931e9be3ede2496376
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-11-08 10:40:19 +0000

    [Finish]2-16

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"  #サイトのルートページを決める記述
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
   get 'homes/about' => 'about#index'
   get '/about' => 'about#index' ,as: 'about'
